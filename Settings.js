450cf76c6a61f41f88f522885b0647c5df7d380e
zeeshuzeeshu281-cmd <zeeshuzeeshu281@gmail.com>
2026-07-29 07:26:53 +0500

    Create Settings.js

---
+++ Settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: '𝐉ᴜɴᴇ 𝐌ᴅ',
+  author: 'supreme',
+  botName: "𝐉ᴜɴᴇ 𝐌ᴅ",
+  botOwner: 'Supreme', // Your name
+  ownerNumber: '254798570132', //Set your number here without + symbol.
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.2.0",
+};
+
+module.exports = settings;
